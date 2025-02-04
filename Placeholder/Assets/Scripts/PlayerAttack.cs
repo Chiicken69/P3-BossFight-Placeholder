b57ebb0f54e49440e549beff9707fa831bf35161
ChristofferJelle <christofferjt@gmail.com>
2025-02-04 13:56:41 +0100

    Made raycast bullet and fucked up plastic SCM

--- Placeholder/Assets/Scripts/PlayerAttack.cs
+++ Placeholder/Assets/Scripts/PlayerAttack.cs
@@ -1,17 +1,25 @@
 using System.Collections.Generic;
 using System.Resources;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerAttack : MonoBehaviour
 {
-    private float attackDamage = 10;
-    private float attackTime = 5;
-    private GameObject bullet;
+
+    // private float attackDamage = 10;
+    private float bulletSpeed = 10;
+    // private float attackTime = 5;
+    public GameObject bulletPrefab;
     private GameObject player;
+    private GameObject fired_bullet;
+    public float game_z = 8;
+
+
 
     void Start()
     {
         player = GameObject.Find("Player");
+ 
     }
 
 
@@ -20,8 +28,47 @@ public class PlayerAttack : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(bullet,player.transform);
+            Vector2 screenPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 playerPos = transform.position;
+            Vector2 shot = screenPos - playerPos;
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, shot, Mathf.Infinity);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.name);
+            }
+
+            Debug.DrawRay(transform.position, shot, Color.red);
         }
     }
 
+
+    /*
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            cloneBullet = Instantiate(bullet, player.transform);
+            directionToShoot = cloneBullet.transform.forward - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            cloneBullet.transform.Translate(directionToShoot * bulletSpeed * Time.deltaTime);
+        }
+
+
     }
+
+
+        Vector2 screen_mouse_pos = Input.mousePosition;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(screen_mouse_pos.x, screen_mouse_pos.y, game_z));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject fired_bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+            Vector3 bullet_direction = new Vector3(mousePos.x, mousePos.y, game_z) - transform.position;
+            fired_bullet.transform.Translate(bullet_direction * bulletSpeed * Time.deltaTime);
+
+            Destroy(fired_bullet,1f);
+        }
+    */
+
+}
