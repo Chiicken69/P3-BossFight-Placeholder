7ef21651d340524887a71bfb710454e7c58bab6a
mixelmaster61 <mixelsemail@gmail.com>
2025-02-04 09:37:01 +0100

    BossfightSetup

---
+++ Placeholder/Assets/Scripts/BossMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+
+    [SerializeField] private int _phase;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (_phase)
+        {
+
+            case 0:
+
+                break;
+
+            default:
+                break;
+        }
+
+    }
+}
