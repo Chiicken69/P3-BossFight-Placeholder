d68b01a9882a8b0edefc4686d1e32caea1255b08
Panthy <adamcola06@gmail.com>
2025-02-04 14:01:12 +0100

    HP system
    isn't integtaed with save system but works in isolation

---
+++ Placeholder/Assets/HealthSystem.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    
+   [SerializeField] private int _currentHealth;
+   [SerializeField] private int _maxHealth;
+
+
+    private void Awake()
+    {
+        InitializeHealth(_maxHealth);
+    }
+
+
+    public void TakeDamage(int DamageToTake)
+    {
+        _currentHealth -= DamageToTake;
+    }
+
+    public void GainHealth(int HealthToGain)
+    {
+        int diff = HealthToGain + _currentHealth;
+
+
+        if (diff < _maxHealth) 
+        {
+            _currentHealth += HealthToGain;
+        }
+        
+    }
+
+    public void InitializeHealth(int hp)
+    {
+       _currentHealth = _maxHealth;
+    } 
+}
