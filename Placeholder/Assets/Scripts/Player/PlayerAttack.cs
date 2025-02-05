3dbbdce6dfc95654c5a9e843ff253782341c209f
ChristofferJelle <christofferjt@gmail.com>
2025-02-05 17:43:59 +0100

    Added ammo system to PlayerAttack

--- Placeholder/Assets/Scripts/Player/PlayerAttack.cs
+++ Placeholder/Assets/Scripts/Player/PlayerAttack.cs
@@ -13,16 +13,22 @@ public class PlayerAttack : MonoBehaviour
     //public GameObject bulletPrefab;
     private GameObject _player;
     private GameObject _firedBullet;
+
     float _gunDuration =0.08f;
+
+    private int _remainingBullets;
 
     LineRenderer gunLine;
 
     [SerializeField]
-    float _timer = 1;
+    float _timer = 0.5f;
 
     [SerializeField]
-    float _resetTimer = 1;
-    
+    float _resetTimer = 0.5f;
+
+    private int currentAmmoLoaded;
+    private int maxAmmoCapacity = 6;
+
     void Start()
     {
         _player = this.gameObject;
@@ -31,13 +37,17 @@ public class PlayerAttack : MonoBehaviour
     }
     void Update()
     {
-      FireShot();  
+      FireShot();
+        Reload();
       _timer -= Time.deltaTime;
     }
 
+    //this funktion uses calculate shot to shoot out a raycast where mouse is clicked and then uses another raycast to check if it actually hit any collider
+    //it wont shoot out a ray if there is no collider to hit.
     private void FireShot()
     {
-        if (Input.GetMouseButtonDown(0) && _timer < 0)
+        Debug.Log(currentAmmoLoaded);
+        if (Input.GetMouseButtonDown(0) && _timer < 0 && currentAmmoLoaded > 0)
         {
           Vector2 shot = CalculateShot();
           RaycastHit2D hit = Physics2D.Raycast(transform.position, shot, Mathf.Infinity);
@@ -53,11 +63,23 @@ public class PlayerAttack : MonoBehaviour
                 Debug.DrawRay(transform.position, shot, Color.red);
             }
             StartCoroutine(ShootGun());
-
+            --currentAmmoLoaded;
             _timer = _resetTimer;
 
 
 
+        }
+    }
+ 
+
+
+    private void Reload()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            int reloadAmount = maxAmmoCapacity - currentAmmoLoaded;
+            reloadAmount = (currentAmmoLoaded + reloadAmount) >= 0 ? reloadAmount : currentAmmoLoaded;
+            currentAmmoLoaded += reloadAmount;
         }
     }
 
