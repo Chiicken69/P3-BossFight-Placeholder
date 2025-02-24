548e63c1fb32fa03fa5a5adaa804928aa5579204
Chiicken69 <adamcola06@gmail.com>
2025-02-24 13:23:01 +0100

    Updated audiomanager script and gameobject

--- Placeholder/Assets/AudioManager.cs
+++ Placeholder/Assets/AudioManager.cs
@@ -1,10 +1,56 @@
+using System;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance;
     public Sound[] musicSounds, sfxSounds;
     public AudioSource musicSource, sfxSource;
 
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
+    public void PlayMusic(string name)
+    {
+        Sound sound = Array.Find(musicSounds, x => x.name == name);
 
+        if (sound == null)
+        {
+            Debug.Log("Sound not found or is null");
+        }
+        else
+        {
+            musicSource.clip = sound.clip;
+            musicSource.Play();
+        }
+    }
+    
+    public void PlaySFX(string name)
+    {
+        Sound sound = Array.Find(sfxSounds, x => x.name == name);
+
+        if (sound == null)
+        {
+            Debug.Log("Sound not found or is null");
+        }
+        else
+        {
+           sfxSource.PlayOneShot(sound.clip);
+        }
+    }
+    
+    
+    
+    
+    
 }
