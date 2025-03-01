552e2ceae9db041f7d6bb19ef02d5043c9a90f9d
ChristofferJelle <christofferjt@gmail.com>
2025-03-01 18:50:17 +0100

    "fixed goon damage" invinibility dont work yet

--- Placeholder/Assets/Scripts/Boss/BossWaveScipts/Goon.cs
+++ Placeholder/Assets/Scripts/Boss/BossWaveScipts/Goon.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Threading;
+using System.Timers;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.EventSystems.EventTrigger;
 using static UnityEngine.GraphicsBuffer;
@@ -16,39 +18,60 @@ public class Goon : MonoBehaviour
     private void Awake()
     {
         Physics2D.IgnoreLayerCollision(7,8);
-        Physics2D.IgnoreLayerCollision(7,2);
+        Physics2D.IgnoreLayerCollision(7, 2);
         Physics2D.IgnoreLayerCollision(7,9);
         _goonRB = GetComponent<Rigidbody2D>();
 
         player = GameObject.Find("Player");
     }
 
+    /*
     void OnCollisionEnter2D(UnityEngine.Collision2D collision)
     {
- 
-    }
-
-    float _speed;
-    Vector2 _target;
-
-    private void FixedUpdate()
-    {
-        print(_speed);
-        _goonRB.AddForce(_target * _speed);
-
-        if (goonerTimer == 0)
-        { 
-        Destroy(this.gameObject);
-        }
-
-        float distance = Vector3.Distance(transform.position, player.transform.position);
-        if (distance < 1.0f) // Threshold for "collision"
+        if (collision.collider.name == "Player") // Threshold for "collision"
         {
             //Output the Collider's GameObject's name
 
             player.GetComponent<HealthSystem>().TakeDamage(goonDamage);
+            
         }
     }
+    */
+    float _speed;
+    Vector2 _target;
+
+    [SerializeField]
+    float _timer = 0f;
+    [SerializeField]
+    float _resetTimer = 1f;
+
+    private void FixedUpdate()
+    {
+        _timer-= Time.deltaTime; 
+
+        print(_speed);
+        _goonRB.AddForce(_target * _speed);
+
+        if (goonerTimer == 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+        if (_timer <= 0) // Only deal damage when the timer has expired
+        {
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+            if (distance < 1.0f) // Threshold for "collision"
+            {
+                player.GetComponent<HealthSystem>().TakeDamage(goonDamage);
+                _timer = _resetTimer; // Reset the timer so damage isn't applied immediately next frame
+            }
+        }
+    }
+
+  
+    
+
 
     public void SetTarget(Vector2 target)
     {
